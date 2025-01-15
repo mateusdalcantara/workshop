773b4ffc069a7e8c322109c7ff363d739c234045
Mateus Alcantara <m_davirap@outlook.com>
2025-01-15 07:34:37 -0300

    Implemented DTO to return Users

--- src/main/java/com/mateusdalcantara/workshop/resources/UserResource.java
+++ src/main/java/com/mateusdalcantara/workshop/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.mateusdalcantara.workshop.resources;
 
 import com.mateusdalcantara.workshop.domain.User;
+import com.mateusdalcantara.workshop.dto.UserDTO;
 import com.mateusdalcantara.workshop.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -18,9 +20,10 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(UserDTO::new).toList();
+        return ResponseEntity.ok().body(listDto);
 
     }
 }
